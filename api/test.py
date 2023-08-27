84a6f002928799e5c0a548468de375b6cb1b3d48
hamza <benfaida.hamza2014@gmail.com>
2023-08-27 18:53:33 +0200

    api django

--- api/test.py
+++ api/test.py
@@ -5,7 +5,7 @@ API_URL = 'http://127.0.0.1:8000'
 AUTH_TOKEN = 'ceb232845ecb1d9423ba385ea49952af4260a588'
 
 def test_list_students():
-    response = requests.get(f'http://127.0.0.1:8000/', headers={'Authorization': f'Token {AUTH_TOKEN}'})
+    response = requests.get(f'{API_URL}/', headers={'Authorization': f'Token {AUTH_TOKEN}'})
     assert response.status_code == 200
 
 if __name__ == '__main__':
